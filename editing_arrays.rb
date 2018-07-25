7600299cca74185e927282dce11c72c64005b2c9
madddanz <maddiedanzberger@gmail.com>
2018-07-25 15:29:19 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -15,16 +15,14 @@ end
 
 change_rainbow_colors
 
+# def add_colors
+#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+#   rainbow_colors = ["yellow","default","light_cyan"]
+#   rainbow_colors [0] = "red"
+#   rainbow_colors [1] = "light_red"
+#   rainbow_colors [2] = "yellow"
+#   rainbow_colors.push "green"
+#   rainbow_colors.push "blue"
+#   return rainbow_colors
+# end
 
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-  rainbow_colors = ["yellow","default","light_cyan"]
-  rainbow_colors [0] = "red"
-  rainbow_colors [1] = "light_red"
-  rainbow_colors [2] = "yellow"
-  rainbow_colors.push "green"
-  rainbow_colors.push "blue"
-   return rainbow_colors
-end
-
-add_colors
