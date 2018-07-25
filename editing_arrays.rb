80e6a8efae72d033aaec94b98d051f650d3e24d5
madddanz <maddiedanzberger@gmail.com>
2018-07-25 15:29:53 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -20,9 +20,10 @@ def add_colors
   rainbow_colors = ["yellow","default","light_cyan"]
   rainbow_colors [0] = "red"
   rainbow_colors [1] = "light_red"
-  rainbow_colors [2] = "yellow"
+  rainbow_colors [2] = "light_yellow"
   rainbow_colors.push "green"
   rainbow_colors.push "blue"
   return rainbow_colors
 end
 
+add_colors
