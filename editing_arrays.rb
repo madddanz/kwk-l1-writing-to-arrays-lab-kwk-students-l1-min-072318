e04a93d49ae6540ae80b0f0533bdb9e55a797ed8
madddanz <maddiedanzberger@gmail.com>
2018-07-25 15:19:41 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,29 +4,29 @@
 #   "light_cyan"
 # ]
 
-# def change_rainbow_colors
-#   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-#   rainbow_colors = ["yellow","default","light_cyan"]
-#   rainbow_colors [0] = "red"
-#   rainbow_colors [1] = "light_red"
-#   rainbow_colors [2] = "yellow"
-#     puts rainbow_colors
-# end
-
-# return change_rainbow_colors
-
-
-
-
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+def change_rainbow_colors
+  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors = ["yellow","default","light_cyan"]
   rainbow_colors [0] = "red"
   rainbow_colors [1] = "light_red"
   rainbow_colors [2] = "yellow"
-  rainbow_colors.push(green)
-  rainbow_colors.push(blue)
-  puts rainbow_colors
+    puts rainbow_colors
 end
 
-return add_colors
+return change_rainbow_colors
+
+
+
+
+# def add_colors
+#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+#   rainbow_colors = ["yellow","default","light_cyan"]
+#   rainbow_colors [0] = "red"
+#   rainbow_colors [1] = "light_red"
+#   rainbow_colors [2] = "yellow"
+#   rainbow_colors.push(green)
+#   rainbow_colors.push(blue)
+#   puts rainbow_colors
+# end
+
+# return add_colors
