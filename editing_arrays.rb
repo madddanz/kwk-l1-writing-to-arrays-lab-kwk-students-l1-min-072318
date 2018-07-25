c8f5cee8106cf6f29578634b6a038352920e3f79
madddanz <maddiedanzberger@gmail.com>
2018-07-25 15:24:19 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -22,8 +22,8 @@ def add_colors
   rainbow_colors [0] = "red"
   rainbow_colors [1] = "light_red"
   rainbow_colors [2] = "yellow"
-    puts rainbow_colors
-  rainbow_colors.push(green)
+  rainbow_colors.push "green"
+  rainbow_colors.push "blue"
     puts rainbow_colors
 end
 
